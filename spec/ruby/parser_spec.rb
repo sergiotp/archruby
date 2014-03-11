b92d2b010604beacba47a8f0d1bae0e0aef285ee
Sergio Henrique <sergiohenriquetp@gmail.com>
2014-03-10 21:53:48 -0300

    add some spec to ruby parser class

--- spec/ruby/parser_spec.rb
+++ spec/ruby/parser_spec.rb
@@ -10,16 +10,25 @@ describe ArchChecker::Ruby::Parser do
     ruby_parser.dependencies.should include("TesteClasse")
     ruby_parser.dependencies.should include("BostaQualquer")
     ruby_parser.dependencies.should include("ClasseDeTeste::Em::OutroModulo::NaPQP")
-    puts ruby_parser.classes_and_dependencies.inspect
   end
   
-  it 'extract correct class from file' do
-    
+  it 'extract correct class from file' do    
     ruby_parser.classes.should include("Teste")
     ruby_parser.classes.should include("BostaOutra")
   end
   
   it 'extract correct classes and depencies' do
+    ruby_parser.classes_and_dependencies.keys.count.should be_eql(2)
+    ruby_parser.classes_and_dependencies['Teste'].count.should be_eql(7)
+    ruby_parser.classes_and_dependencies['BostaOutra'].count.should be_eql(1)
+    
+    teste_class_dependencies = ruby_parser.classes_and_dependencies['Teste'].collect { |dependency| dependency.class_name}
+    bosta_outra_class_dependencies = ruby_parser.classes_and_dependencies['BostaOutra'].collect { |dependency| dependency.class_name}
+    
+    teste_class_dependencies.should include("ClasseDeTeste::Em::OutroModulo::NaPQP")
+    teste_class_dependencies.should include("BostadeTeste::Testado")
+    
+    bosta_outra_class_dependencies.should include("BostaQualquer")
   end
   
 end
