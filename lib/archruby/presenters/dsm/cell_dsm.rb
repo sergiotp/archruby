f89015659eb3ecdd7b9ec63b89f8c6f3007926a1
Elderjr <elderjr1995@gmail.com>
2015-11-19 10:36:24 -0200

    Improvements in the appearance of DSM

--- lib/archruby/presenters/dsm/cell_dsm.rb
+++ lib/archruby/presenters/dsm/cell_dsm.rb
@@ -8,4 +8,16 @@ class CellDSM
     @type = type
   end
 
+  def how_many_access_with_font
+    font = 
+      if how_many_access >= 1000
+        "<font size='1'>#{@how_many_access}<font>"
+      elsif how_many_access >= 100
+        "<font size='2'>#{@how_many_access}<font>"
+      else
+        "#{@how_many_access}"
+      end
+    font
+  end
+
 end
