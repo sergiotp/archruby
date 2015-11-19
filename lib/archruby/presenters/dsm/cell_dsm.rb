8cbf802b3a919a418b831279a44419eb8cf6e082
Sergio Henrique <sergiohenriquetp@gmail.com>
2015-11-19 13:54:06 -0200

    fixing class namespace in cell_dsm

--- lib/archruby/presenters/dsm/cell_dsm.rb
+++ lib/archruby/presenters/dsm/cell_dsm.rb
@@ -1,23 +1,29 @@
-class CellDSM
+module Archruby
+  module Presenters
 
-  attr_reader :type
-  attr_reader :how_many_access
+    class CellDSM
 
-  def initialize(how_many_access, type)
-    @how_many_access = how_many_access
-    @type = type
-  end
+      attr_reader :type
+      attr_reader :how_many_access
 
-  def how_many_access_with_font
-    font = 
-      if how_many_access >= 1000
-        "<font size='1'>#{@how_many_access}<font>"
-      elsif how_many_access >= 100
-        "<font size='2'>#{@how_many_access}<font>"
-      else
-        "#{@how_many_access}"
+      def initialize(how_many_access, type)
+        @how_many_access = how_many_access
+        @type = type
       end
-    font
-  end
 
+      def how_many_access_with_font
+        font =
+          if how_many_access >= 1000
+            "<font size='1'>#{@how_many_access}<font>"
+          elsif how_many_access >= 100
+            "<font size='2'>#{@how_many_access}<font>"
+          else
+            "#{@how_many_access}"
+          end
+        font
+      end
+
+    end
+
+  end
 end
